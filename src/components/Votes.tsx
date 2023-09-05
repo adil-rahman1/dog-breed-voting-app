8604e7dd0fa836e6f601ccebbf4fef30e117c8f8
cynthiacodes <cjnkstudy01@hotmail.com>
2023-09-05 15:53:55 +0100

    posting breedname to db using handleVote function

--- src/components/Votes.tsx
+++ src/components/Votes.tsx
@@ -1,15 +1,18 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-
+import { extractDogBreedName } from "../extractDogBreedName";
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 interface IBreedsToCompare {
-    breedOne: string;
-    breedTwo: string;
+    breedOneUrl: string;
+    breedTwoUrl: string;
 }
 
+const baseUrl = "https://dog-breed-voting-c7b1.onrender.com";
 export function Votes(): JSX.Element {
     const [breedsToCompare, setBreedsToCompare] = useState<IBreedsToCompare>({
-        breedOne: "",
-        breedTwo: "",
+        breedOneUrl: "",
+        breedTwoUrl: "",
     });
 
     const getAndStoreTwoRandomBreeds = async () => {
@@ -17,8 +20,11 @@ export function Votes(): JSX.Element {
             const response = await axios.get(
                 "https://dog.ceo/api/breeds/image/random/2"
             );
-            const [breedOne, breedTwo] = response.data.message;
-            setBreedsToCompare({ breedOne, breedTwo });
+            const [breedOneUrl, breedTwoUrl] = response.data.message;
+            setBreedsToCompare({
+                breedOneUrl,
+                breedTwoUrl,
+            });
         } catch (error) {
             console.error(error);
         }
@@ -27,18 +33,31 @@ export function Votes(): JSX.Element {
         getAndStoreTwoRandomBreeds();
     }, []);
 
-    useEffect(() => {
-        console.log("breeds to compare:", breedsToCompare);
-    }, [breedsToCompare]);
+    const handleVote = async (votedBreedUrl: string) => {
+        const breedName = extractDogBreedName(votedBreedUrl);
+        try {
+            await axios.post(`${baseUrl}/breeds/${breedName}`);
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
     return (
         <>
             <h2>Pick your favourite</h2>
             <div>
-                <img src={breedsToCompare.breedOne} alt="" />
+                <img
+                    onClick={() => handleVote(breedsToCompare.breedOneUrl)}
+                    src={breedsToCompare.breedOneUrl}
+                    alt=""
+                />
             </div>
             <div>
-                <img src={breedsToCompare.breedTwo} alt="" />
+                <img
+                    onClick={() => handleVote(breedsToCompare.breedTwoUrl)}
+                    src={breedsToCompare.breedTwoUrl}
+                    alt=""
+                />
             </div>
         </>
     );
