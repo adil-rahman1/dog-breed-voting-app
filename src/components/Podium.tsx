242d8ef063237f1807263be426e8024cc0d70769
TomOwen93 <mr.tsowen@gmail.com>
2023-09-06 11:28:57 +0100

    specific breed img, img css, refresh imgs button

--- src/components/Podium.tsx
+++ src/components/Podium.tsx
@@ -1,7 +1,10 @@
 import { useEffect, useState } from "react";
 import { BreedInfo } from "./Leaderboard";
-import { subBreedDash } from "../utils/checkSubBreed";
-import { Button } from "@chakra-ui/react";
+import { Button, Text } from "@chakra-ui/react";
+import {
+    getSpecificBreedImage,
+    getTop3BreedImages,
+} from "../utils/getBreedImages";
 
 interface PodiumProps {
     top3Breeds: BreedInfo[];
@@ -9,23 +12,25 @@ interface PodiumProps {
 export function Podium({ top3Breeds }: PodiumProps): JSX.Element {
     const [top3BreedImages, setTop3BreedImages] = useState<string[]>([]);
 
-    const getTop3BreedImages = async () => {
-        const breedImagesToUpdate = [];
-        for (const dog of top3Breeds) {
-            const response = await fetch(
-                `https://dog.ceo/api/breed/${subBreedDash(
-                    dog.breedname
-                )}/images/random`
-            );
-            const jsonBody = await response.json();
-            const img = jsonBody.message;
-            breedImagesToUpdate.push(img);
-        }
-        setTop3BreedImages(breedImagesToUpdate);
+    const getAndStoreTop3BreedImages = async () => {
+        setTop3BreedImages(await getTop3BreedImages(top3Breeds));
     };
+
+    const getAndStoreSpecificBreedImage = async (leaderboardRank: number) => {
+        const newBreedImage = await getSpecificBreedImage(
+            leaderboardRank,
+            top3Breeds
+        );
+        setTop3BreedImages((prev) => {
+            const newTop3BreedImages = [...prev];
+            newTop3BreedImages[leaderboardRank] = newBreedImage;
+            return newTop3BreedImages;
+        });
+    };
+
     useEffect(
         () => {
-            getTop3BreedImages();
+            getAndStoreTop3BreedImages();
         },
         // eslint-disable-next-line
         []
@@ -36,39 +41,47 @@ export function Podium({ top3Breeds }: PodiumProps): JSX.Element {
             <Button
                 colorScheme="teal"
                 margin={"2vh"}
-                onClick={getTop3BreedImages}
+                onClick={getAndStoreTop3BreedImages}
             >
                 Refresh Top 3 Breed Images
             </Button>
             <div className="container podium">
                 <div className="podium__item">
-                    <img
-                        src={top3BreedImages[1]}
-                        alt={top3Breeds[1].breedname}
-                    ></img>
-                    <p className="podium__breed">
-                        {top3Breeds[1].breedname} - {top3Breeds[1].votes} Votes
-                    </p>
+                    <button onClick={() => getAndStoreSpecificBreedImage(1)}>
+                        <Text as="b">
+                            {top3Breeds[1].breedname} - {top3Breeds[1].votes}{" "}
+                            Votes
+                        </Text>
+                        <img
+                            className="breed-image"
+                            src={top3BreedImages[1]}
+                            alt={top3Breeds[1].breedname}
+                        ></img>
+                    </button>
                     <div className="podium__rank second">🥈</div>
                 </div>
                 <div className="podium__item">
-                    <img
-                        src={top3BreedImages[0]}
-                        alt={top3Breeds[0].breedname}
-                    ></img>
-                    <p className="podium__breed">
+                    <Text as="b">
                         {top3Breeds[0].breedname} - {top3Breeds[0].votes} Votes
-                    </p>
+                    </Text>
+                    <button onClick={() => getAndStoreSpecificBreedImage(0)}>
+                        <img
+                            src={top3BreedImages[0]}
+                            alt={top3Breeds[0].breedname}
+                        ></img>
+                    </button>
                     <div className="podium__rank first">🥇</div>
                 </div>
                 <div className="podium__item">
-                    <img
-                        src={top3BreedImages[2]}
-                        alt={top3Breeds[2].breedname}
-                    ></img>
-                    <p className="podium__breed">
+                    <Text as="b">
                         {top3Breeds[2].breedname} - {top3Breeds[2].votes} Votes
-                    </p>
+                    </Text>
+                    <button onClick={() => getAndStoreSpecificBreedImage(2)}>
+                        <img
+                            src={top3BreedImages[2]}
+                            alt={top3Breeds[2].breedname}
+                        ></img>
+                    </button>
                     <div className="podium__rank third">🥉</div>
                 </div>
             </div>
